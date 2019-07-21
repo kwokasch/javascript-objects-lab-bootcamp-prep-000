526b04f6b61e19e15a4c3fcf14c3b04d926b6844
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 18:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object({})
 
 var obj = { prop: 1}
 
-var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+var newObj = Object.assign({}, obj)
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value})
