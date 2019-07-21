a1f62d25078b3facfaa958c41cc0587d3ca11031
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 18:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var obj = { prop: 1}
 
 var newObj = Object.assign({}, obj)
 
-var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value})
 }
