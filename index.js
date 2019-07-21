ea4fd251fdac77b51426a2ffd1757a47223466a7
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 18:25:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object({})
 
 var obj = { prop: 1}
 
-function updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(recipes, key, value){
   object[prop1] = 2
   return obj
 }
