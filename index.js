2a9835cb9de3363d183bc9f51c2dd2b887f36cfd
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 18:32:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = new Object({})
 var obj = { prop: 1}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, obj, {key: value})
+  return Object.assign({}, obj, {[key]: value})
 }
 
